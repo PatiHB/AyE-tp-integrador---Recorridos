db1f64792ab3821ab17f6a3956ff47134dc219d8
lordcardamomo <lordcardamomo@cachyos-x8664>
2026-05-28 18:52:21 -0300

    cambios sobre la carga de usuarios/autos

--- AyEtpi/src/clases/Mapa.java
+++ AyEtpi/src/clases/Mapa.java
@@ -1,20 +1,46 @@
 package clases;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import grafoDirigido.*;
 
 public class Mapa extends GrafoDirigido{
 	private ListaAutos autos;
 	private ListaUsuarios usuarios;
-	Map<String, Integer> puntosUnicos;
+	private Map<String, Integer> puntosUnicos;
 	
 	public Mapa(int orden) {
 		super(orden);
 		autos=new ListaAutos();
 		usuarios=new ListaUsuarios();
+		puntosUnicos=null;
 	}
 	
+	public void agregarAuto(String posicion) {
+		if(puntosUnicos.containsKey(posicion)) {
+			autos.insertar(new Auto(posicion), 0);
+		}
+		else {
+			System.out.println("agregarAuto: La posición no existe");
+		}
+	}
+	public void agregarUsuario(String posicion) {
+		if(puntosUnicos.containsKey(posicion)) {
+			usuarios.insertar(new Usuario(posicion), 0);
+		}
+		else {
+			System.out.println("agregarUsuario: La posición no existe");
+		}
+	}
 	
+	public void eliminarUsuario(int pos) {
+		autos.eliminar(pos);
+	}
+	public void eliminarAuto(int pos) {
+		usuarios.eliminar(pos);
+	}
+	
+	public void cargarHash(Map<String, Integer> M) {
+		puntosUnicos=M;
+	}
 }
